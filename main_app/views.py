db1b589575db9fdbd15bc3b0d4e5949dfadefc03
anthowest <anthowest14@gmail.com>
2022-10-10 14:15:51 -0500

    Add home template, render template view

--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 # Create your views here.
 
-class Home(View):
-
-    def get(self, request):
-        return HttpResponse("ConnectCare Home")
+class Home(TemplateView):
+    template_name = "home.html"
 
 
 class About(View):
