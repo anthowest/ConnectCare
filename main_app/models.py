4cbafa31b9a8ee1f8db3b138d1b0ffb46e11b59b
anthowest <anthowest14@gmail.com>
2022-10-10 15:35:06 -0500

    Create Provider model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Provider(models.Model):
+
+    name = models.CharField(max_length=100)
+    speciality = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
