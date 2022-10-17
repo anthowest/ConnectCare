0d9638abdb89d6465bd598b7815be275d76e4eba
anthowest <anthowest14@gmail.com>
2022-10-17 08:39:56 -0500

    Removed About, changed records display

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
-    path('about/', views.About.as_view(), name="about"),
+    # path('about/', views.About.as_view(), name="about"),
     path('patients/', views.PatientList.as_view(), name="patient_list"),
     path('patients/new/', views.PatientCreate.as_view(), name="patient_create"),
     path('patients/<int:pk>/', views.PatientDetail.as_view(), name="patient_detail"),
